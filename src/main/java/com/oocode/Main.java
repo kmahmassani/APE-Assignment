805aafbb1ff1a9112febceacbf43983fb4e9c8fb
Kamal Mahmassani <kmahamassani@gmail.com>
2017-08-01 14:03:26 +0300

    Rename variables for readability using IDE refactoring rename

--- src/main/java/com/oocode/Main.java
+++ src/main/java/com/oocode/Main.java
@@ -4,21 +4,21 @@ import okhttp3.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        int n = Integer.parseInt(args[2]);  // the number of windows of this size
-        int w = Integer.parseInt(args[0]);  // the width of the window
-        int h = Integer.parseInt(args[1]);  // the height of the window
-        String r = args[3];                 // the model name of these windows
+        int quantity = Integer.parseInt(args[2]);  // the number of windows of this size
+        int windowWidth = Integer.parseInt(args[0]);  // the width of the window
+        int windowHeight = Integer.parseInt(args[1]);  // the height of the window
+        String windowModel = args[3];                 // the model name of these windows
         OkHttpClient client = new OkHttpClient();
 
         // the thickness of the frame depends on the model of window
-        int width = width(r, true); int height = width(r, false);
+        int width = width(windowModel, true); int height = width(windowModel, false);
 
-        RequestBody requestBody = BodyBuilder.bodyBuilder(w, h, n, width, height);
-        if (h > 120) requestBody = BodyBuilder.bodyBuilder2(w, h, n, width, height);
+        RequestBody requestBody = BodyBuilder.bodyBuilder(windowWidth, windowHeight, quantity, width, height);
+        if (windowHeight > 120) requestBody = BodyBuilder.bodyBuilder2(windowWidth, windowHeight, quantity, width, height);
 
         // the glass pane is the size of the window minus allowance for
         // the thickness of the frame
-        if ((w-width) * (h-height) * n > 20000) {
+        if ((windowWidth-width) * (windowHeight-height) * quantity > 20000) {
             Request request = new Request.Builder()
                     .url("https://immense-fortress-19979.herokuapp.com/large-order")
                     .method("POST", RequestBody.create(null, new byte[0]))
